335e7ba79cdbc4cd9152ca015ef4e7430307010e
Gladwin-Paul <87390026+Gladwin-Paul@users.noreply.github.com>
2021-08-17 03:01:02 -0400

    Create ip_configuration.ps1
    Lab 3

---
+++ Powershell/ip_configuration.ps1
@@ -0,0 +1,9 @@
+﻿get-ciminstance win32_networkadapterconfiguration | 
+where-object IPEnabled -eq $True |
+select-object description, 
+@{n="Index";e={$_.index}},
+@{n="IP Address";e={$_.ipaddress}},
+@{n="Subnet Mask";e={$_.IPSubnet}},
+@{n="DNS Domain Name";e={$_.DNSHostName}},
+@{n="DNS Server";e={$_.DNSServerSearchOrder}} |
+format-table 
